b120b295daedae5742ca4a3ede5fd77d69e9a32e
warham194 <warham194@gmail.com>
2017-10-05 17:09:56 +0300

    task 189

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,15 @@
+package ru.job4j.max;
+
+public class Max {
+
+  public int max(int first, int second) {
+    int result;
+   // return ( first > second ? first : second);
+         if (first > second){
+		 result = first;
+		 } else result = second;
+		 return result;
+		 
+		
+  }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+
+
+public class MaxTest {
+@Test
+public void whenFirstLessSecond() {
+    Max maxim = new Max();
+    int result = maxim.max(1, 2);
+    assertThat(result, is(2));
+ }
+}
