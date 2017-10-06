59e05c8c4fa927d8f9463445991c2372597ba0aa
warham194 <warham194@gmail.com>
2017-10-06 19:59:59 +0300

    task 9461

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,42 @@
+package ru.job4j.condition;
+
+
+
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
+    public double distance(Point left, Point right) {
+        return Math.sqrt(Math.pow((left.getX() - right.getX()) + Math.pow(left.getY() - right.getY()));
+    }
+
+    public double period(double ab, double ac, double bc) {
+        return -1;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = this.distance(this.a, this.b);
+        double ac = this.distance(this.a, this.c);
+        double bc = this.distance(this.b, this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+
+            rsl = Math.sqrt(p *(p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+
+
+    private boolean exist(double ab, double ac, double bc) {
+        return false;
+    }
+}
