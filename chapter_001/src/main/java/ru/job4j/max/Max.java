39ec2da139959db6f6daed04bc4ea95114c3e9eb
warham194 <warham194@gmail.com>
2017-10-07 19:04:44 +0300

    task 190

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,15 +1,16 @@
 package ru.job4j.max;
 
 public class Max {
-
-  public int max(int first, int second) {
     int result;
-   // return ( first > second ? first : second);
+    public int max(int first, int second, int third)  {
+	return max(max(first, second), third)
+	
+	public int max(int first, int second) {
          if (first > second){
 		 result = first;
 		 } else result = second;
 		 return result;
 		 
-		
+	}	
   }
 }
