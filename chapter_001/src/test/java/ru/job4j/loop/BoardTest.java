ca6920110abff711def1f58c13d3014ebc3e0215
warham194 <warham194@gmail.com>
2018-02-20 19:06:45 +0300

    внес правки

--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -2,6 +2,7 @@ package ru.job4j.loop;
 
 import org.junit.Test;
 
+import static java.lang.System.lineSeparator;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
@@ -19,7 +20,7 @@ public class BoardTest {
         Board board = new Board();
         String result = board.paint(3, 3);
         final String separator = System.getProperty("line.separator");
-        String expected = String.format("x x%s x %sx x%s", separator, separator, separator);
+        String expected = String.format("x x%s x %sx x%s", lineSeparator(), lineSeparator(), lineSeparator());
         assertThat(result, is(expected));
     }
 
@@ -32,7 +33,7 @@ public class BoardTest {
         Board board = new Board();
         String result = board.paint(5, 4);
         final String separator = System.getProperty("line.separator");
-        String expected = String.format("x x x%s x x %sx x x%s x x %s", separator, separator, separator, separator);
+        String expected = String.format("x x x%s x x %sx x x%s x x %s", lineSeparator(), lineSeparator(), lineSeparator(), lineSeparator());
         assertThat(result, is(expected));
     }
 }
