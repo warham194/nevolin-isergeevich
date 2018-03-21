1f37d87710c121556ac266e601dfd0ff51ae4aa9
warham194 <warham194@gmail.com>
2018-03-21 16:45:13 +0300

    Создать итератор четные числа [#150]

---
+++ chapter_002/src/main/java/ru/ilya/iteraror/IteratorArrayEven.java
@@ -0,0 +1,56 @@
+package ru.ilya.iteraror;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Lenovo2 on 21.03.2018.
+ */
+public class IteratorArrayEven implements Iterator<Integer> {
+
+    private final int[] value;
+    private int index = 0;
+
+    public IteratorArrayEven(int[] value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        /*
+        if (Arrays.equals(value, new int[]{})) {   проверка должна быть в методе next() или hasNext()?
+            throw new NoSuchElementException();
+        }
+         */
+        if (index < value.length - 1) {
+            for (int a = index; a < value.length; a++) {
+                if (value[a] % 2 == 0) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Integer next() {
+        if (Arrays.equals(value, new int[]{})) {
+            throw new NoSuchElementException();
+        }
+        int result = 0;
+        for (int i = index; i < value.length; i++) {
+            if (value[i] % 2 == 0) {
+                result = value[i];
+                break;
+            }
+            if (hasNext() == false) {
+                throw new NoSuchElementException();
+            }
+            index++;
+        }
+        return result;
+    }
+}
---
+++ chapter_002/src/test/java/ru/ilya/iteraror/IteratorArrayEvenTest.java
@@ -0,0 +1,66 @@
+package ru.ilya.iteraror;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lenovo2 on 21.03.2018.
+ */
+public class IteratorArrayEvenTest {
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new IteratorArrayEven(new int[]{1, 2, 3, 4, 5, 6, 7});
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnEvenNumbersSequentially() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));  //
+        assertThat(it.hasNext(), is(true));  // все 3 работают правильно
+        assertThat(it.hasNext(), is(true));  //
+        assertThat(it.next(), is(2));  // выполняется правильно
+        assertThat(it.next(), is(4));  // тут возникает ошибка
+        assertThat(it.next(), is(6));
+    }
+
+    @Test
+    public void  shouldReturnFalseIfNoAnyEvenNumbers() {
+        it = new IteratorArrayEven(new int[]{1});
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void allNumbersAreEven() {
+        it = new IteratorArrayEven(new int[]{2, 4, 6, 8});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
+    }
+
+
+}
