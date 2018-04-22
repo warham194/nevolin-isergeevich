a381e7ff6c2d25dcf773d38ab91626db96ecb131
warham194 <warham194@gmail.com>
2018-04-22 19:55:52 +0300

    Создать контейнер на базе связанного списка  [#159]

---
+++ chapter_002/src/main/java/ru/ilya/iteraror/LinkedSimple.java
@@ -0,0 +1,104 @@
+package ru.ilya.iteraror;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Lenovo2 on 20.04.2018.
+ */
+public class LinkedSimple<E> implements Iterable<E> {
+
+    private Node<E> first = new Node<E>(0, null, null, null);
+    private Node<E> second = new Node<E>(0, null, null, null);
+    private int size = 0;
+    private int counter = 0;
+
+    /**
+     * Метод добавления элемента в список.
+     * @param element
+     */
+    public void add(E element) {
+        Node node = new Node(size++, element, second, null);
+        if (first.next == null) {
+            first.next = node;
+        } else {
+            second.next = node;
+        }
+        second = node;
+        counter++;
+    }
+
+    public E get(int index) {
+
+            E result = null;
+            Node node = second;
+            for (int i = 0; i < this.size; i++) {
+                if (index == node.index) {
+                    result = (E) node.value;
+                } else {
+                    node = node.previous;
+                }
+            }
+            return result;
+        }
+
+
+    private class Node<E> {
+        private E value;
+        public int index;
+        private Node previous;
+        private Node next;
+
+        /**
+         * Конструктор
+         * @param index
+         * @param value
+         * @param next
+         * @param previous
+         */
+        public Node(int index, E value, Node previous, Node next) {
+            this.value = value;
+            this.next = next;
+            this.previous = previous;
+            this.index = index;
+        }
+    }
+
+
+
+
+
+    @Override
+    public Iterator<E> iterator() {
+
+            Node nod = first.next;
+            int expectedModCount = counter;
+
+            return new Iterator<E>() {
+                private Node node = nod;
+
+                @Override
+                public boolean hasNext() throws ConcurrentModificationException {
+                    if (expectedModCount != counter) {
+                        throw new ConcurrentModificationException();
+                    }
+
+                    return node != null;
+                }
+
+                @Override
+                public E next() throws NoSuchElementException {
+                    if (!this.hasNext()) {
+                        throw new NoSuchElementException();
+                    }
+
+                    E result = (E) node.value;
+                    node = node.next;
+                    return result;
+                }
+            };
+
+    }
+}
---
+++ chapter_002/src/test/java/ru/ilya/iteraror/LinkedSimpleTest.java
@@ -0,0 +1,65 @@
+package ru.ilya.iteraror;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+/**
+ * Created by Lenovo2 on 20.04.2018.
+ */
+public class LinkedSimpleTest {
+
+    private LinkedSimple linkedList;
+    private Iterator iterator;
+    private String obj1;
+    private String obj2;
+    private String obj3;
+
+    @Before
+    public void setUp() {
+        linkedList = new LinkedSimple();
+        obj1 = "example1";
+        obj2 = "example2";
+        obj3 = "example3";
+        linkedList.add(obj1);
+        linkedList.add(obj2);
+        linkedList.add(obj3);
+        iterator = linkedList.iterator();
+    }
+
+    @Test
+    public void whenGetObjects() {
+        assertThat(linkedList.get(0), is(obj1));
+        assertThat(linkedList.get(1), is(obj2));
+        assertThat(linkedList.get(2), is(obj3));
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void whenIteratorHasNextAndNext() {
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(obj1));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(obj2));
+        assertThat(iterator.next(), is(obj3));
+        assertThat(iterator.hasNext(), is(false));
+        iterator.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIteratorNextThenCME() {
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(obj1));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(obj2));
+        linkedList.add("mod");
+        iterator.hasNext();
+    }
+
+}
