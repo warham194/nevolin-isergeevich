82aa8bf94f3e40c8a8304c257f0763d0e6757ac0
warham194 <warham194@gmail.com>
2018-05-03 13:34:17 +0300

    Реализовать коллекцию Set на массиве

---
+++ chapter_002/src/main/java/ru/ilya/set/SimpleSet.java
@@ -0,0 +1,50 @@
+package ru.ilya.set;
+
+import ru.ilya.list.ArraySimple;
+
+import java.util.Iterator;
+
+/**
+ * Created by Lenovo2 on 03.05.2018.
+ */
+
+public class SimpleSet<E> implements Iterable<E> {
+
+    private final ArraySimple<E> glass = new ArraySimple();
+    /**
+     * Метод добавления элемента без дубликатов
+     * @param e
+     */
+    public void add(E e) {
+        if (dooblicate(e)) {
+            glass.add(e);
+        }
+    }
+
+
+    public E get(int index) {
+        return glass.get(index);
+    }
+    /**
+     * Проверка на дубоикаты
+     * @param e
+     * @return
+     */
+    private boolean dooblicate(E e) {
+        boolean result = true;
+        if (e != null) {
+            for (E item : glass) {
+                if (e.equals(item)) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return glass.iterator();
+    }
+}
---
+++ chapter_002/src/test/java/ru/ilya/set/SimpleSetTest.java
@@ -0,0 +1,56 @@
+package ru.ilya.set;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.ilya.list.ArraySimple;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lenovo2 on 03.05.2018.
+ */
+public class SimpleSetTest {
+    private SimpleSet simpleSet;
+    private Iterator iterator;
+    private String obj1;
+    private String obj2;
+    private String obj3;
+
+    @Before
+    public void setUp() {
+        simpleSet = new SimpleSet();
+        obj1 = "example1";
+        obj2 = "example2";
+        obj3 = "example3";
+        simpleSet.add(obj1);
+        simpleSet.add(obj2);
+        simpleSet.add(obj3);
+        iterator = simpleSet.iterator();
+    }
+
+    @Test
+    public void whenAddObjectsThenGetThisObjects() {
+        assertThat(simpleSet.get(0), is(obj1));
+        assertThat(simpleSet.get(1), is(obj2));
+        assertThat(simpleSet.get(2), is(obj3));
+    }
+
+    @Test
+    public void whenAddDuplicateThenNotAdd() {
+        assertThat(simpleSet.get(0), is("example1"));
+        assertThat(simpleSet.get(1), is("example2"));
+        assertThat(simpleSet.get(2), is("example3"));
+        assertThat(simpleSet.get(3) == null, is(true));
+        simpleSet.add("example1");
+        simpleSet.add("example3");
+        assertThat(simpleSet.get(3) == null, is(true));
+    }
+
+
+}
