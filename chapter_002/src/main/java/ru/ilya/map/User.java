f8a4a42173d71ac33aa1f749901da1e8e56f6061
warham194 <warham194@gmail.com>
2018-05-07 12:45:47 +0300

    Переопределить только hashCode

--- chapter_002/src/main/java/ru/ilya/map/User.java
+++ chapter_002/src/main/java/ru/ilya/map/User.java
@@ -15,4 +15,14 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;  // проверяем , что name не равно null (если равно , то результат 0)
+                                                          // в ином случае при помощи метода высчитываем hashCode переменной name
+
+        result = 31 * result + children;                  // для оптимального распределения хеш значения умножим на 31 и добавим значение второй переменной
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0); // аналогично вычислим хеш значение для birthday
+        return result;
+    }
 }
--- chapter_002/src/test/java/ru/ilya/map/UserMapTest.java
+++ chapter_002/src/test/java/ru/ilya/map/UserMapTest.java
@@ -11,17 +11,19 @@ import java.util.Map;
  * Created by Lenovo2 on 06.05.2018.
  */
 public class UserMapTest {
-    private Map<User,Object> map = new HashMap<>();
+    private Map<User, Object> map = new HashMap<>();
     private Calendar birthday = new GregorianCalendar(1994, 4, 10);
 
-    private User first = new User("Ilya",0,birthday);
-    private User second = new User("Ilya", 0,birthday);
+    private User first = new User("Ilya", 0, birthday);
+    private User second = new User("Ilya", 0, birthday);
 
     @Test
     public void whenAddUsersAndNotUseOverriddenEquals() {
-        map.put(first,1);
-        map.put(second,2);
-        System.out.println(map);
+        map.put(first, 1);
+        map.put(second, 2);
+        System.out.println(map); //при переопределении hashCode программа считает объекты разными
+                                  // т.к. совпадение хеш не значит , что объекты индентичны
+                                 // в данном случае мы получим два объекта с одинаковыми хеш значениями и разными ключами
 
     }
 
