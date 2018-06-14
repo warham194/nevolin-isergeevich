2be65611e916182b10d4f7b5e7074ef010babb0a
warham194 <warham194@gmail.com>
2018-06-14 11:16:32 +0300

    поправил сравнение двух слов

--- chapter_002/src/main/java/ru/ilya/kontrol/Simile.java
+++ chapter_002/src/main/java/ru/ilya/kontrol/Simile.java
@@ -2,6 +2,8 @@ package ru.ilya.kontrol;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Lenovo2 on 25.05.2018.
@@ -9,16 +11,40 @@ import java.util.Arrays;
 public class Simile {
     public boolean comWords(String firstWord, String secondWord) {
         boolean result = firstWord.length() == secondWord.length();
+        /* Первый способ
             char[] chArrayOne = firstWord.toCharArray();
             char[] chArrayTwo = secondWord.toCharArray();
             Arrays.sort(chArrayOne);
             Arrays.sort(chArrayTwo);
-            for (int i = 0; i < chArrayOne.length; i++) {
-                if (chArrayOne[i] != chArrayTwo[i]) {
-                    result = false;
-                    break;
-                }
+            if(!Arrays.equals(chArrayOne, chArrayTwo)) {
+              result = false;
             }
+            */
+        // Второй способ
+        char[] chArrayOne = firstWord.toCharArray();
+        char[] chArrayTwo = secondWord.toCharArray();
+        Map<Character, Integer> anagram = new HashMap<>();
+        for (Character c : chArrayOne) {
+            int count = 1;
+            if (anagram.containsKey(c)) {
+                count = anagram.get(c) + 1;
+            }
+            anagram.put(c, count);
+        }
+        for (Character c : chArrayTwo) {
+            int count = - 1;
+            if (anagram.containsKey(c)) {
+                count = anagram.get(c) - 1;
+            }
+            anagram.put(c, count);
+        }
+
+        for (Character c : anagram.keySet()) {
+            if (anagram.get(c) != 0) {
+                result = false;
+                break;
+            }
+        }
         return result;
     }
 
