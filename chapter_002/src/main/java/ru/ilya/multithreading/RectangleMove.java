4013a0373da4d0afbe598b6c783250a3152c0db7
warham194 <warham194@gmail.com>
2018-08-03 19:45:59 +0300

    Пинг-Понг

---
+++ chapter_002/src/main/java/ru/ilya/multithreading/RectangleMove.java
@@ -0,0 +1,33 @@
+package ru.ilya.multithreading;
+
+/**
+ * Created by Lenovo2 on 03.08.2018.
+ */
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+    private final int maxX;
+
+    public RectangleMove(Rectangle rect, int maxX) {
+        this.rect = rect;
+        this.maxX = maxX;
+    }
+
+    @Override
+    public void run() {
+        int x = 1;
+        while (true) {
+            if (this.rect.getX() <= 0 || this.rect.getX() >= maxX) {
+                x *= -1;
+            }
+                this.rect.setX(this.rect.getX() + x);
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
